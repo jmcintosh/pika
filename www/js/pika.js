0709be75fea7e7cd0c76a69188a98380c9d0b0e4
jmcintosh <jmcintosh4s@gmail.com>
2015-12-19 20:32:26 -0700

    scaling, some text
    figured out scaling, added some text but i don't like how it looks.

--- www/js/pika.js
+++ www/js/pika.js
@@ -1,19 +1,35 @@
+var film = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, render: render });
 
-var film = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, render: render });
+var basicTextStyle = { font: "18px Arial", 
+    fill: "black", 
+    wordWrap: true, 
+    wordWrapWidth: window.innerWidth/5, 
+    align: "center" };
 
 var scenes = [ 
     {'title': 'chrome',
-     'url': 'video/chrome.webm'
+     'url': 'video/chrome.webm',
+     'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id dolor commodo, laoreet dui in, interdum urna.'
     },
     {'title': 'liquid',
-     'url': 'video/liquid2.mp4'
+     'url': 'video/liquid2.mp4',
+     'text': 'There use to be a video of a flaming skull, but I found it tacky so I removed it.'
     },
     {'title': 'wormhole',
-     'url': 'video/wormhole.mp4'
+     'url': 'video/wormhole.mp4',
+     'text': "This video won't loop for some reason. Perhaps we will never know why."
     }
 ];
 
+var speed = 1; //change speed of video, for testing purposes
+var scene = 0; //initial scene
+
 function preload() {
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+    this.scale.forceOrientation(true,false);
+    //film.scale.setScreenSize( true );
 
     scenes.forEach(function(item,index,array) {
         film.load.video(item.title,item.url);
@@ -21,20 +37,26 @@ function preload() {
 
 }
 
-//var video;
-//var image;
-var speed = 5;
-var scene = 0;
-
 function create() {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
     
     scenes.forEach(function(item,index,array) {
         var video = film.add.video(item.title);
-        //video.onPlay.addOnce(start,this);
         item.video = video;
-        item.image = video.addToWorld(400, 300, 0.5, 0.5);  
+        var scalex = width/video.video.videoWidth;
+        var scaley = height/video.video.videoHeight;
+        var scale = Math.min(scalex,scaley);
+        item.image = video.addToWorld(width/2,height/2,0.5,0.5,scale,scale);
+        if( item.hasOwnProperty('text') ){
+            console.log("adding text\n");
+            text = film.add.text(-100,50, item.text, basicTextStyle);
+            text.anchor.set(0.5);
+            item.image.addChild(text);
+        }
         item.image.kill();
     });
+    
     scenes[0].video.onPlay.addOnce(start,this);
     scenes[0].image.revive();
     scenes[0].video.play(true,speed);
@@ -58,15 +80,16 @@ function changeSource() {
     scenes[scene].image.revive();
     scenes[scene].video.play(true,speed);
     
-
 }
+
+
 
 function render() {
 
-    film.debug.text("Video width: " + scenes[scene].video.video.videoWidth, 600, 32);
-    film.debug.text("Video height: " + scenes[scene].video.video.videoHeight, 600, 64);
+    //film.debug.text("Video width: " + scenes[scene].video.video.videoWidth, 600, 32);
+    //film.debug.text("Video height: " + scenes[scene].video.video.videoHeight, 600, 64);
 
-    film.debug.text("Video Time: " + scenes[scene].video.currentTime, 32, 32);
-    film.debug.text("Video Duration: " + scenes[scene].video.duration, 32, 64);
+    //film.debug.text("Video Time: " + scenes[scene].video.currentTime, 32, 32);
+    //film.debug.text("Video Duration: " + scenes[scene].video.duration, 32, 64);
 
 }
